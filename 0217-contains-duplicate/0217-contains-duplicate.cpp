7cdbcc583278ced6b3c2f254bb650e6ce792ee46
Tejas Choudhari <143324250+tejaschoudhari25@users.noreply.github.com>
2026-02-12 19:15:45 +0530

    Time: 109 ms (6.17%), Space: 95 MB (5.85%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>st;
+        for(int i = 0; i < nums.size(); i++){
+            st.insert(nums[i]);
+        }
+        if(st.size() == nums.size()){
+            return false;
+        }
+        return true;
+    }
+};
